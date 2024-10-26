5a67a121bc6cffeae6b24562aa4c1bb6a09be103
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-10-26 16:14:30 -0400

    refined search engine

--- search-engine.py
+++ search-engine.py
@@ -25,29 +25,65 @@ def searchFor(searchPhrase, catListings):
     res.append(prio['prio4'])
     return res
 
-def advancedSearch(catListings, productPhrase, locationPhrase = None):
+def advancedSearch(catListings, productPhrase, locationPhrase = None, have = None):
     '''
     advancedSearch returns the prefered listing based on advanced search
     '''
 
     #checking if advanced search is even required
-    if not location:
+    if not locationPhrase and not have:
         return searchFor(productPhrase, catListings)
-    
-    productPhrase = productPhrase.lower()
-    locationPhrase = locationPhrase.lower()
-    prio = {'prio1': [], 'prio2': [], 'prio3': []}
-    #They are filtered by priority, so the higher priority is put first on a list.
-    for listing in catListings:
-        if productPhrase == listing.product_name.lower() and locationPhrase == listing.location.lower():
-            prio['prio1'].append(listing)
-        elif productPhrase in listing.product_name.lower() and locationPhrase in listing.location.lower():
-            prio['prio2'].append(listing)
-        elif productPhrase in listing.description.lower() or locationPhrase in listing.description.lower():
-            prio['prio3'].append(listing)
-    
-    res = []
-    res.append(prio['prio1'])
-    res.append(prio['prio2'])
-    res.append(prio['prio3'])
-    return res
+    if not have:
+        productPhrase = productPhrase.lower()
+        locationPhrase = locationPhrase.lower()
+        prio = {'prio1': [], 'prio2': [], 'prio3': []}
+        #They are filtered by priority, so the higher priority is put first on a list.
+        for listing in catListings:
+            if productPhrase == listing.product_name.lower() and locationPhrase == listing.location.lower():
+                prio['prio1'].append(listing)
+            elif productPhrase in listing.product_name.lower() and locationPhrase in listing.location.lower():
+                prio['prio2'].append(listing)
+            elif productPhrase in listing.description.lower() or locationPhrase in listing.description.lower():
+                prio['prio3'].append(listing)
+        
+        res = []
+        res.append(prio['prio1'])
+        res.append(prio['prio2'])
+        res.append(prio['prio3'])
+        return res
+    elif not locationPhrase:
+        productPhrase = productPhrase.lower()
+        have = have.lower()
+        prio = {'prio1': [], 'prio2': []}
+
+        #They are filtered by priority, so the higher priority is put first on a list.
+        for listing in catListings:
+            if productPhrase == listing.product_name.lower() and have == listing.user.need:
+                prio['prio1'].append(listing)
+            elif productPhrase in listing.product_name.lower() and have in listing.user.need:
+                prio['prio2'].append(listing)
+        
+        res = []
+        res.append(prio['prio1'])
+        res.append(prio['prio2'])
+        return res
+    else:
+        productPhrase = productPhrase.lower()
+        have = have.lower()
+        locationPhrase = locationPhrase.lower()
+        prio = {'prio1': [], 'prio2': [], 'prio3': []}
+
+        #They are filtered by priority, so the higher priority is put first on a list.
+        for listing in catListings:
+            if productPhrase == listing.product_name.lower() and have == listing.user.need.lower() and locationPhrase == listing.location.lower():
+                prio['prio1'].append(listing)
+            elif productPhrase in listing.product_name.lower() and have in listing.user.need.lower() and locationPhrase in listing.location.lower():
+                prio['prio2'].append(listing)
+            elif productPhrase in listing.product_name.lower() and (have in listing.user.need.lower() or locationPhrase in listing.location.lower()):
+                prio['prio3'].append(listing)
+        
+        res = []
+        res.append(prio['prio1'])
+        res.append(prio['prio2'])
+        res.append(prio['prio3'])
+        return res
