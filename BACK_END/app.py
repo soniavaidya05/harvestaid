258a8b077bd0c5d754815543da09ca370eab5573
soniavaidya05 <sonia.vaidya05@gmail.com>
2024-10-27 03:10:32 -0400

    image folder and app.py

--- BACK_END/app.py
+++ BACK_END/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -14,7 +14,17 @@ def index():
 @app.route('/listings', methods=['GET'])
 def get_products():
     products = load_products()
+    category = request.args.get('category')  # Get the category from query parameters
     return jsonify(products)
+
+@app.route('/listings/<int:listing_id>', methods=['GET'])
+def product_details(listing_id):
+    products = load_products()
+    listing = next((p for p in products if p['product name'] == listing_id), None)
+    if listing:
+        return render_template('productPage.html', listing=listing)
+    else:
+        return "Product not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
