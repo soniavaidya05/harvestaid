a1248415a4bf5a719ca477b1cb07630d2e1c70b3
Nathan Romero <schmutts@yahoo.ca>
2024-10-27 05:08:58 -0400

    Created login page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Chewy } from "next/font/google";
 
 const chewyFont = Chewy({ subsets: ["latin"], weight: "400" });
@@ -9,20 +10,22 @@ export default function Home() {
       style={{ backgroundColor: "rgb(245, 244, 227)" }}
       className="flex h-screen flex-col"
     >
-      <div className="flex justify-center items-center relative w-full h-full bg-[#b2c0b0] drop-shadow-md">
+      <div className="flex justify-center items-center relative bg-[#b2c0b0] drop-shadow-md">
         <Image
           src="/HarvestAidText.png"
           alt="Harvest Aid Text"
-          layout="fill"
+          width={2300}
+          height={500}
           className="my-auto objection-contain"
         />
       </div>
       <div className="flex justify-center h-2/3">
-        <button
+        <Link
           className={`mx-auto my-auto px-5 py-2 text-3xl text-white bg-[#b2c0b0] border-b-4 border-[#8b9c8d] rounded transition ease-in-out transform hover:translate-y-1 hover:border-0 ${chewyFont.className}`}
+          href="/login"
         >
           Get Started
-        </button>
+        </Link>
       </div>
     </div>
   );
