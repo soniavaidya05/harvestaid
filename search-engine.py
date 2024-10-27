5a657eeccfc29db80c19bdef55db9f3a5facdb02
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-10-26 21:01:06 -0400

    fixed search-engine

--- search-engine.py
+++ search-engine.py
@@ -31,60 +31,60 @@ def advancedSearch(catListings, productPhrase, locationPhrase = None, have = Non
     advancedSearch returns the prefered listing based on advanced search
     '''
 
-#checking if advanced search is even required
-if not locationPhrase and not have:
-    return searchFor(productPhrase, catListings)
-if not have:
-    productPhrase = productPhrase.lower()
-    locationPhrase = locationPhrase.lower()
-    prio = {'prio1': [], 'prio2': [], 'prio3': []}
-    #They are filtered by priority, so the higher priority is put first on a list.
-    for listing in catListings:
-        if productPhrase == listing.product_name.lower() and locationPhrase == listing.location.lower():
-            prio['prio1'].append(listing)
-        elif productPhrase in listing.product_name.lower() and locationPhrase in listing.location.lower():
-            prio['prio2'].append(listing)
-        elif productPhrase in listing.description.lower() or locationPhrase in listing.description.lower():
-            prio['prio3'].append(listing)
+    #checking if advanced search is even required
+    if not locationPhrase and not have:
+        return searchFor(productPhrase, catListings)
+    if not have:
+        productPhrase = productPhrase.lower()
+        locationPhrase = locationPhrase.lower()
+        prio = {'prio1': [], 'prio2': [], 'prio3': []}
+        #They are filtered by priority, so the higher priority is put first on a list.
+        for listing in catListings:
+            if productPhrase == listing.product_name.lower() and locationPhrase == listing.location.lower():
+                prio['prio1'].append(listing)
+            elif productPhrase in listing.product_name.lower() and locationPhrase in listing.location.lower():
+                prio['prio2'].append(listing)
+            elif productPhrase in listing.description.lower() or locationPhrase in listing.description.lower():
+                prio['prio3'].append(listing)
 
-    res = []
-    res.extend(prio['prio1'])
-    res.extend(prio['prio2'])
-    res.extend(prio['prio3'])
-    return res
-elif not locationPhrase:
-    productPhrase = productPhrase.lower()
-    have = have.lower()
-    prio = {'prio1': [], 'prio2': []}
+        res = []
+        res.extend(prio['prio1'])
+        res.extend(prio['prio2'])
+        res.extend(prio['prio3'])
+        return res
+    elif not locationPhrase:
+        productPhrase = productPhrase.lower()
+        have = have.lower()
+        prio = {'prio1': [], 'prio2': []}
 
-    #They are filtered by priority, so the higher priority is put first on a list.
-    for listing in catListings:
-        if productPhrase == listing.product_name.lower() and have == listing.user.need:
-            prio['prio1'].append(listing)
-        elif productPhrase in listing.product_name.lower() and have in listing.user.need:
-            prio['prio2'].append(listing)
+        #They are filtered by priority, so the higher priority is put first on a list.
+        for listing in catListings:
+            if productPhrase == listing.product_name.lower() and have == listing.user.need:
+                prio['prio1'].append(listing)
+            elif productPhrase in listing.product_name.lower() and have in listing.user.need:
+                prio['prio2'].append(listing)
 
-    res = []
-    res.extend(prio['prio1'])
-    res.extend(prio['prio2'])
-    return res
-else:
-    productPhrase = productPhrase.lower()
-    have = have.lower()
-    locationPhrase = locationPhrase.lower()
-    prio = {'prio1': [], 'prio2': [], 'prio3': []}
+        res = []
+        res.extend(prio['prio1'])
+        res.extend(prio['prio2'])
+        return res
+    else:
+        productPhrase = productPhrase.lower()
+        have = have.lower()
+        locationPhrase = locationPhrase.lower()
+        prio = {'prio1': [], 'prio2': [], 'prio3': []}
 
-    #They are filtered by priority, so the higher priority is put first on a list.
-    for listing in catListings:
-        if productPhrase == listing.product_name.lower() and have == listing.user.need.lower() and locationPhrase == listing.location.lower():
-            prio['prio1'].append(listing)
-        elif productPhrase in listing.product_name.lower() and have in listing.user.need.lower() and locationPhrase in listing.location.lower():
-            prio['prio2'].append(listing)
-        elif productPhrase in listing.product_name.lower() and (have in listing.user.need.lower() or locationPhrase in listing.location.lower()):
-            prio['prio3'].append(listing)
+        #They are filtered by priority, so the higher priority is put first on a list.
+        for listing in catListings:
+            if productPhrase == listing.product_name.lower() and have == listing.user.need.lower() and locationPhrase == listing.location.lower():
+                prio['prio1'].append(listing)
+            elif productPhrase in listing.product_name.lower() and have in listing.user.need.lower() and locationPhrase in listing.location.lower():
+                prio['prio2'].append(listing)
+            elif productPhrase in listing.product_name.lower() and (have in listing.user.need.lower() or locationPhrase in listing.location.lower()):
+                prio['prio3'].append(listing)
 
-    res = []
-    res.extend(prio['prio1'])
-    res.extend(prio['prio2'])
-    res.extend(prio['prio3'])
-    return res
+        res = []
+        res.extend(prio['prio1'])
+        res.extend(prio['prio2'])
+        res.extend(prio['prio3'])
+        return res
