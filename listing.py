4a39e17d9125e26b4ca9b7745b755037d36784ac
Maira <mairamasroor05@gmail.com>
2024-10-27 04:12:42 -0400

    implemented the pictures for the layout!

--- listing.py
+++ listing.py
@@ -19,10 +19,12 @@ class Listing:
     location: str
     description: str
     username: str
+    image_url: str
 
-    def __init__(self, username: str, category: str, product_name: str, location: str, description: str):
+    def __init__(self, username: str, category: str, product_name: str, location: str, description: str, image_url:str):
         self.username = username
         self.category = staticvariables.choose_category(category)
         self.product_name = product_name 
         self.location = location
         self.description = description
+        self.image_url= image_url
